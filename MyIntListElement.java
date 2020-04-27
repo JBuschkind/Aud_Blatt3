70eecae90c52d48cd56a5c57507a1df7e7f2ba4b
Johann <buschjohann@gmail.com>
2020-04-27 11:35:26 +0200

    Aufgabe 2.a)

--- MyIntListElement.java
+++ MyIntListElement.java
@@ -5,45 +5,42 @@ package ueb03;
  *
  * @author werner, braun (IFIS)
  */
-public class MyIntListElement {    
-    
+public class MyIntListElement {
+
 	private int key;
 	protected MyIntListElement next;
-	
+
 	public MyIntListElement(int key, MyIntListElement next){
 		this.key = key;
 		this.next = next;
 	}
-	
+
 	public MyIntListElement getNext(){
 		return this.next;
 	}
-	
+
 	public void setNext(MyIntListElement next){
 		this.next = next;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param oElement
 	 * @return 0 if both Element equal, -1 if this < oElement, else 1
 	 */
 	public int compare(MyIntListElement oElement){
-		//  _____   _____   _____   _____  
-		// |_   _| /  _  \ |  _  \ /  _  \ 
-		//   | |   | | | | | | | | | | | | 
-		//   | |   | | | | | | | | | | | | 
-		//   | |   | |_| | | |_| | | |_| | 
-		//   |_|   \_____/ |_____/ \_____/ 
+
+    if(this.key == oElement.key){
+      return 0;
+    }else if(this.key < oElement.key){
+      return -1;
+    } else{
+      return 1;
+    }
 	}
-	
+
 	public String toString(){
-		//  _____   _____   _____   _____  
-		// |_   _| /  _  \ |  _  \ /  _  \ 
-		//   | |   | | | | | | | | | | | | 
-		//   | |   | | | | | | | | | | | | 
-		//   | |   | |_| | | |_| | | |_| | 
-		//   |_|   \_____/ |_____/ \_____/ 
+		return Integer.toString(this.key);
 	}
-	
+
 }
